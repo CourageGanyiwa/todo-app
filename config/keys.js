199ef0fcc5ceab0f0b57ca54f267526448cebfec
Christian Courage <Couresma@gmail.com>
2025-07-26 23:15:17 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,1 @@
-module.exports = {
-  mongoProdURI:
-    "mongodb+srv://couresma:LxVSNM5OZRdlydtd@cluster0.zto5p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-};
+module.exports = {};
